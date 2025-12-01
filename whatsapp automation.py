5c322ef71b2e03919f30c3564b3faa2fb2b3fc4f
chandan9334 <chandanpatel9334@gmail.com>
2025-12-01 18:27:40 +0530

    Create whatsapp automation.py

---
+++ whatsapp automation.py
@@ -0,0 +1,4 @@
+import pywhatkit
+pywhatkit.sendwhatmsg_instantly("+917079236634","Hi its me")
+pywhatkit.sendwhatmsg("+917079236634","Hi its me",10,20)
+pywhatkit.sendwhatmsg_to_group("KYVew3VgcgeKyJfKn1iaDn","Hi its me",19,8)
